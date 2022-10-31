476a6881463b76fb612d1cbae5ab95603475d787
HungPham269 <shinpham909@gmail.com>
2022-11-01 02:05:18 +0700

    up code len

---
+++ BtvnDay2NangCao/Login2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Login2ViewController.swift
+//  BtvnDay2NangCao
+//
+//  Created by Phương Hùng on 01/11/2022.
+//
+
+import UIKit
+
+class Login2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
